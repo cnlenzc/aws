221f71ae048c06bb1f36da9ddbb00c61cf9ded09
Carlos Lenz <cnlenzc@gmail.com>
2023-07-20 14:53:30 +0000

    config connection

---
+++ docs/install/postgres.md
@@ -0,0 +1,5 @@
+sudo yum install postgresql15
+sudo yum install postgresql-devel
+sudo yum install make
+sudo yum install g++
+sudo yum install gcc*
--- servers/crud-postgres/util/db.js
+++ servers/crud-postgres/util/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.PG_URL,
-  { dialect: 'postgres', logging: false });
+  {
+    dialect: 'postgres',
+    logging: false,
+    native: false,
+    dialectOptions: {
+     ssl: false
+    }
+  })
 
 module.exports = sequelize;
